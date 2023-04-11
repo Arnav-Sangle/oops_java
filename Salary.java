88b4e0f6941f61384b4dc5848abd06decbe9da85
Arnav-Sangle <90949554+Arnav-Sangle@users.noreply.github.com>
2023-04-11 10:55:45 +0530

    error due to in.close() is corrected

--- Salary.java
+++ Salary.java
@@ -10,11 +10,11 @@ public class Salary {
 		Team_Lead obj2 = new Team_Lead();
 		Assistant_Project_Manager obj3 = new Assistant_Project_Manager();
 		Project_Manager obj4 = new Project_Manager();
-		
+		double basicPay;
 		int ch;
 		do {
 			
-			System.out.printf("%n******MENU******%n");
+			System.out.printf("%n%n******MENU******%n");
 			System.out.printf("1. Programmer\n");
 			System.out.printf("2. Team Lead\n");
 			System.out.printf("3. Assistant Project Manager\n");
@@ -26,12 +26,46 @@ public class Salary {
 			
 			switch(ch) {
 			case 1:	
-				System.out.printf()
+				System.out.printf("Enter basic pay: ");
+				basicPay = in.nextDouble();
+				obj1 = new Programmer(basicPay);
 				obj1.getData();
+//				System.out.print(basicPay);
+//				obj1 = new Programmer(basicPay);	//object creation may override existing execution so do it beforehand
 				obj1.displayData();
-					
-					
-					
+				obj1.displaySal();
+				break;
+				
+			case 2:	
+				System.out.printf("Enter basic pay: ");
+				basicPay = in.nextDouble();
+				obj2 = new Team_Lead(basicPay);
+				obj2.getData();
+				obj2.displayData();
+				obj2.displaySal();
+				break;
+			
+			case 3:	
+				System.out.printf("Enter basic pay: ");
+				basicPay = in.nextDouble();
+				obj3 = new Assistant_Project_Manager(basicPay);
+				obj3.getData();
+				obj3.displayData();
+				obj3.displaySal();
+				break;
+				
+			case 4:	
+				System.out.printf("Enter basic pay: ");
+				basicPay = in.nextDouble();
+				obj4 = new Project_Manager(basicPay);
+				obj4.getData();
+				obj4.displayData();
+				obj4.displaySal();
+				break;
+				
+			case 0:
+				System.out.printf("Exit Successful");
+				break;
 			}
 		
 		}while(ch != 0);
@@ -44,46 +78,43 @@ public class Salary {
 
 
 class Employee {
-	String emp_name;
 	int emp_id;
-	String address;
+	String emp_name;
 	String mail_id;
+	String address;
 	long mobile_no;
 	
 	void getData() {
 		Scanner in = new Scanner(System.in);
 		
-		System.out.println("Enter Employee data:-\n");
+		System.out.printf("\nEnter Employee data:-\n");
 		
-		System.out.println("Enter ID: ");
-		emp_id = in.nextInt();
+		System.out.printf("Enter ID: ");
+		this.emp_id = in.nextInt();		//for string input
 		
-		System.out.println("Enter Name: ");
-		emp_name= in.nextLine();
+		System.out.printf("Enter Name: ");
+		this.emp_name = in.next();
 		
-		System.out.println("Enter E-mail ID: ");
-		mail_id= in.nextLine();
+		System.out.printf("Enter E-mail ID: ");
+		this.mail_id= in.next();
 		
-		System.out.println("Enter Address: ");
-		address= in.nextLine();
+		System.out.printf("Enter Address: ");
+		this.address= in.next();
 		
-		System.out.println("Enter Mobile No: ");
-		mobile_no= in.nextLong();
+		System.out.printf("Enter Mobile No: ");
+		this.mobile_no= in.nextLong();
 		
-		in.close();
+//		in.close();
 	}
 	
 	void displayData() {
-		Scanner in = new Scanner(System.in);
-		
-		System.out.printf("Employee Details :-\n");
+		System.out.print("\nEmployee Details :-\n");
 		System.out.printf("\tEmployee ID: %d", emp_id);
 		System.out.printf("\tName: %s", emp_name);
 		System.out.printf("\tE-mail ID: %s", mail_id);
-		System.out.printf("\tAddress: %s", address);
-		System.out.printf("\tMobile No: %ld", mobile_no);
-		
-		in.close();
+		System.out.print("\tAddress:"+address);
+		System.out.print("\tMobile No:"+mobile_no);
+
 	}
 	
 }
@@ -104,7 +135,7 @@ class Programmer extends Employee {
 		net = 0;
 	}
 	
-	Programmer (double basicPay) {
+	Programmer(double basicPay) {
 		bp = basicPay;
 		da = bp * 97/100;
 		hra = bp * 10/100;
@@ -115,7 +146,7 @@ class Programmer extends Employee {
 	}
 	
 	void displaySal() {
-		System.out.printf("Programmer Salary Details -\n");
+		System.out.printf("\nProgrammer Salary Details -\n");
 		System.out.printf("\tBasic Pay: %.2f\n", bp);
 		System.out.printf("\tDearness Allowance: %.2f", da);
 		System.out.printf("\tHouse Rent Allowance:  %.2f", hra);
@@ -153,7 +184,7 @@ class Team_Lead extends Employee {
 	}
 
 	void displaySal() {
-		System.out.printf("Programmer Salary Details -\n");
+		System.out.printf("Team Lead Salary Details -\n");
 		System.out.printf("\tBasic Pay: %.2f\n", bp);
 		System.out.printf("\tDearness Allowance: %.2f", da);
 		System.out.printf("\tHouse Rent Allowance:  %.2f", hra);
@@ -191,7 +222,7 @@ class Assistant_Project_Manager extends Employee {
 	}
 	
 	void displaySal() {
-		System.out.printf("Programmer Salary Details -\n");
+		System.out.printf("Assistant Project Manager Salary Details -\n");
 		System.out.printf("\tBasic Pay: %.2f\n", bp);
 		System.out.printf("\tDearness Allowance: %.2f", da);
 		System.out.printf("\tHouse Rent Allowance:  %.2f", hra);
@@ -229,7 +260,7 @@ class Project_Manager extends Employee {
 	}
 	
 	void displaySal() {
-		System.out.printf("Programmer Salary Details -\n");
+		System.out.printf("Project Manager Salary Details -\n");
 		System.out.printf("\tBasic Pay: %.2f\n", bp);
 		System.out.printf("\tDearness Allowance: %.2f", da);
 		System.out.printf("\tHouse Rent Allowance:  %.2f", hra);
